16390addf2ef04578337b4501f5ae54e827c8fff
ameyxd <ameyambade@gmail.com>
2015-06-15 12:11:33 +0530

    dsafas

---
+++ Integrations/SlackIntegration/src/slack/channels.java
@@ -0,0 +1,13 @@
+package slack;
+
+import com.teamchat.client.annotations.OnKeyword;
+import com.teamchat.client.sdk.TeamchatAPI;
+
+public class channels {
+
+	public void channelAdd(TeamchatAPI api)
+	{
+		
+	}
+	
+}
---
+++ Integrations/SlackIntegration/src/slack/test.java
@@ -0,0 +1,12 @@
+package slack;
+
+import com.teamchat.client.annotations.OnAlias;
+import com.teamchat.client.annotations.OnMsg;
+import com.teamchat.client.sdk.TeamchatAPI;
+
+public class test {
+	@OnMsg
+	public void Msg(TeamchatAPI api){
+		
+	}
+}
