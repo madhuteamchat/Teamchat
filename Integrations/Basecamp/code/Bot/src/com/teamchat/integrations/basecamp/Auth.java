3bd28d303a1d149e7901df41a8646bf12c161a89
puru1joy <puru1joy@gmail.com>
2015-06-16 13:03:20 +0530

    updates fixes

---
+++ Integrations/Basecamp/code/Bot/src/com/teamchat/integrations/basecamp/Auth.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.teamchat.integrations.basecamp;
+
+/**
+ * @author Puranjay Jain
+ *
+ */
+public class Auth {
+
+	
+	
+	//test
+	public static void main(String args[]){
+		
+	}
+}
