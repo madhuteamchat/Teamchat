f867cd831c293cd59bfd2afaad0e17e491b33945
puru1joy <puru1joy@gmail.com>
2015-06-17 12:04:18 +0530

    more changes

---
+++ Integrations/Basecamp/code/Server/src/com/teamchat/integrations/basecamp/Token.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.teamchat.integrations.basecamp;
+
+/**
+ * @author Puranjay Jain
+ *
+ */
+public class Token {
+	private String expires_in,refresh_token,access_token;
+}
