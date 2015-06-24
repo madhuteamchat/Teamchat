241eb14371b069112aca90f0d7700328a274405c
walaywarrior <anujarora.web2@gmail.com>
2015-06-24 11:04:27 +0530

    second commit

--- Integrations/Anuj Arora/Zendesk/ZendeskBot/src/com/teamchat/integration/zendesk/main/Bot.java
+++ Integrations/Anuj Arora/Zendesk/ZendeskBot/src/com/teamchat/integration/zendesk/main/Bot.java
@@ -1,6 +1,6 @@
 package com.teamchat.integration.zendesk.main;
 
-//Using zendesk client form - https://github.com/cloudbees/zendesk-java-client (maven project added)
+
 import java.io.FileInputStream;
 import java.util.Properties;
 
@@ -13,15 +13,9 @@ import org.zendesk.client.v2.model.Ticket;
  import org.zendesk.client.v2.model.Trigger.Condition;
  import org.zendesk.client.v2.model.Trigger.Conditions;
  import org.zendesk.client.v2.model.Action;
- import org.zendesk.client.v2.model.targets.UrlTarget;
+ import org.zendesk.client.v2.model.tarsets.UrlTarget;
  */
 
-
-
-
-
-
-// Teamchat sdk
 import com.teamchat.client.annotations.OnAlias;
 import com.teamchat.client.annotations.OnKeyword;
 import com.teamchat.client.sdk.Form;
@@ -29,13 +23,16 @@ import com.teamchat.client.sdk.TeamchatAPI;
 import com.teamchat.client.sdk.chatlets.PrimaryChatlet;
 
 public class Bot {
+	
 	String roomId, formId, ticketId, requesterId, comment;
 	Zendesk zd;
 	long tarId;
 	DBHandler ticketData = new DBHandler();
 
-	@OnKeyword("support")
+	@OnKeyword("tcsupport")
 	public void support(TeamchatAPI api) {
+		
+		
 		zd = new Zendesk.Builder("https://teamchat.zendesk.com") // Zendesk account id
 				.setUsername("savio.dmello@teamchat.com") // email of zendesk account
 				.setPassword("webaroo").build(); // or setToken("0cf1d94606")
@@ -46,9 +43,10 @@ public class Bot {
 		Form f = api.objects().form();
 		f.addField(api.objects().input().label("Subject").name("subject"));
 		f.addField(api.objects().input().label("Comment").name("comment"));
-
+		f.addField(api.objects().input().label("Contact No.").name("contact"));
+		
 		PrimaryChatlet prime = new PrimaryChatlet();
-		prime.setQuestion("Enter your query").setReplyScreen(f)
+		prime.setQuestionHtml("<h4><b>Ticketing Service</b></h4>"+"<img src=\"http://p3.zdassets.com/hc/settings_assets/481529/200038422/Q2xRKEc55HY0Oxq8hxzRaw-teamchatsupportlogo.png\">"+"<br /><b>Enter your query below:</b>").setReplyScreen(f)
 				.setReplyLabel("Enter").setDetailsLabel("Queries")
 				.showDetails(true).alias("query");
 
@@ -63,7 +61,8 @@ public class Bot {
 
 		String sub = api.context().currentReply().getField("subject");
 		String com = api.context().currentReply().getField("comment");
-
+     	String con = api.context().currentReply().getField("contact");
+     	
 		// Setting comment
 		Comment c = new Comment();
 		c.setBody(com);
@@ -71,7 +70,7 @@ public class Bot {
 		// Creating a new ticket, administrator privilege is required
 		Ticket ticket = new Ticket();
 		ticket.setSubject(sub);
-		ticket.setComment(c     );
+		ticket.setComment(c);
 
 		ticket = zd.createTicket(ticket);
 		ticketId = String.valueOf(ticket.getId());
@@ -83,7 +82,7 @@ public class Bot {
 		f.addField(api.objects().input().label("Comment").name("upCom"));
 
 		PrimaryChatlet prime = new PrimaryChatlet();
-		prime.setQuestionHtml("<ul><li>Ticket - " + ticketId + "</li><li>Subject - " + sub + "</li><li>Comment - " + com + "</li></ul>")
+		prime.setQuestionHtml("<h4><b>Ticket Details:</b></h4>"+"<ul type=\"square\"; style=\"color:#359FD8\";><li><a1 style=\"color:black\";><b>Ticket ID- </b></a1><a2 style=\"color:#359FD8\";>" + ticketId + "</a2></li><li><a3 style=\"color:black\";><b>Requester ID- </b></a3><a4 style=\"color:#359FD8\";>" + requesterId + "</a4></li><li><a5 style=\"color:black\";><b>Subject - </b></a5><a6 style=\"color:#359FD8\";>" + sub + "</a6></li><li><a7 style=\"color:black\";><b>Comment - </b></a7><a8 style=\"color:#359FD8\";>" + com + "</a8></li></ul>")
 				.setReplyScreen(f).setReplyLabel("Update")
 				.setDetailsLabel("Update").showDetails(true).alias("update");
 
@@ -93,10 +92,10 @@ public class Bot {
 		formId = prime.getFormId();
 		comment = com;
 		
-		System.out.println("TICKET ID"+ticketId);
-		System.out.println("REQUESTER ID"+requesterId);
-		System.out.println("FORM ID"+formId);
-		System.out.println("ROOM ID"+roomId);
+		//System.out.println("TICKET ID"+ticketId);
+	//	System.out.println("REQUESTER ID"+requesterId);
+	//	System.out.println("FORM ID"+formId);
+	//	System.out.println("ROOM ID"+roomId);
 		
 		ticketData.setData(roomId, formId, ticketId, requesterId, comment);
 
