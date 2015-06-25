dff1388eedc47c9250dfa897be8a9e4e83a2c5b6
ameyxd <ameyambade@gmail.com>
2015-06-25 17:24:34 +0530

    Compiled Servlet and main java class to the same web project. Migrated the bot to remote server and made a few minor tweaks.

---
+++ Integrations/Slack/code/Bot/Slack/src/com/teamchat/integrations/slack/slack_auth.java
@@ -0,0 +1,102 @@
+package com.teamchat.integrations.slack;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+import java.util.Properties;
+
+/**
+ * Servlet implementation class slack_auth
+ */
+@WebServlet("/slack_auth")
+public class slack_auth extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public slack_auth() {
+	
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	
+	//	process(request, response);
+	
+		System.out.println(request.getParameter("code"));
+		String code = request.getParameter("code");
+		System.err.println("in servlet");
+		System.out.println(request.getHeaderNames());
+		
+		File file = new File("code.properties");
+		
+		// Save the code in a properties file
+		try {
+			Properties prop = new Properties();
+			prop.setProperty("code", code);
+			FileOutputStream fileOut = new FileOutputStream(file);
+			prop.store(fileOut, null);
+			fileOut.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		String token = request.getParameter("token");
+		System.out.println(request.getRequestURI());
+		System.out.println(token + "in do");
+		
+		//process(request, response);
+	}
+
+	public void process(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		System.out.println(request.getParameter("code"));
+		String code = request.getParameter("code");
+		System.err.println("inside servlet");
+		System.out.println(request.getHeaderNames());
+		
+		// Save the code in a properties file
+		try {
+			Properties prop = new Properties();
+			prop.setProperty("code", code);
+			File file = new File("code.properties");
+			FileOutputStream fileOut = new FileOutputStream(file);
+			prop.store(fileOut, null);
+			fileOut.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
