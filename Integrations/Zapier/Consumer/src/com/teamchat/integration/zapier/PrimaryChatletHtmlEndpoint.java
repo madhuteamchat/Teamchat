d9648e2820d9ddb9b0af04c762abac2bbbd53eed
puru1joy <puru1joy@gmail.com>
2015-07-03 12:40:00 +0530

    Zapier test build (preserved for future use)

---
+++ Integrations/Zapier/Consumer/src/com/teamchat/integration/zapier/PrimaryChatletHtmlEndpoint.java
@@ -0,0 +1,46 @@
+package com.teamchat.integration.zapier;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimaryChatletHtmlEndpoint
+ */
+@WebServlet(description = "the primary chatlet data drop point", urlPatterns = { "/PrimaryChatletHtmlEndpoint" })
+public class PrimaryChatletHtmlEndpoint extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PrimaryChatletHtmlEndpoint() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("hello");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println(request);
+		System.out.println(request.toString());
+	}
+
+}
