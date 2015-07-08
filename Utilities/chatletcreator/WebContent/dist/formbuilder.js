5ff31a9a47e85f1b6c672fb7da3822df4fdf2203
Prakhar Vashsiht <prakhar888.v@gmail.com>
2015-07-08 16:41:36 +0530

    Prakhar Vashisht: Final Commit for Chatlet Creator.

--- Utilities/chatletcreator/WebContent/dist/formbuilder.js
+++ Utilities/chatletcreator/WebContent/dist/formbuilder.js
@@ -753,7 +753,7 @@
     order: 45,
     view: "<div class='input-line'>\n  <span class='above-line'>$</span>\n  <span class='dolars'>\n    <input type='text' />\n    <label>Dollars</label>\n  </span>\n  <span class='above-line'>.</span>\n  <span class='cents'>\n    <input type='text' />\n    <label>Cents</label>\n  </span>\n</div>",
     edit: "",
-   addButton: "<span class=\"symbol\"><span class=\"fa fa-usd\"></span></span> Price"
+    addButton: "<span class=\"symbol\"><span class=\"fa fa-usd\"></span></span> Price"
   });
 
 }).call(this);
--- Utilities/chatletcreator/src/chatletcreator/PrintChatlet.java
+++ Utilities/chatletcreator/src/chatletcreator/PrintChatlet.java
@@ -1,20 +1,25 @@
 package chatletcreator;
 
-
+import com.teamchat.client.annotations.OnKeyword;
 import com.teamchat.client.sdk.Room;
 import com.teamchat.client.sdk.TeamchatAPI;
+import com.teamchat.client.sdk.chatlets.PrimaryChatlet;
 import com.teamchat.client.sdk.chatlets.TextChatlet;
 import com.teamchat.client.sdk.impl.TeamchatAPIImpl;
+import com.teamchat.client.utils.TeamchatContextListener;
 
 public class PrintChatlet
 {
-
+	public static TeamchatAPI api;
+	
+	@OnKeyword("create")
+	public void onCreate(TeamchatAPI api){
+		this.api=api;
+		api.performPostInCurrentRoom(new PrimaryChatlet().setQuestionHtml("<html><body><button onclick='myFunction()'>Click here</button><script>function myFunction(){window.open('http://interns.teamchat.com:8086/chatletcreator/Chatlet-main.html');} </script></body></html>"));
+	}
+	
 	public void set(String output,String email)throws Exception
 	{
-		TeamchatAPI api = TeamchatAPIImpl.fromFile("teamchat1.data")
-				.setEmail("bot888@outlook.com")
-				.setPassword("bazingacooper04");
-
 		Room r=api.context().create().setName("p2p").add(email);
 		api.perform(r.post(new TextChatlet(output)));
 
