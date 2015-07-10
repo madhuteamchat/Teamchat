0820ca9892d75689ba98de72070bee54939f9f73
JosephArnold <xjoseph.arnold@gmail.com>
2015-07-10 11:44:15 +0530

    Committed

--- Integrations/Enchant/src/com/teamchat/integrations/enchant/bot/HostDomain.java
+++
@@ -1,9 +0,0 @@
-package com.teamchat.integrations.enchant.bot;
-
-public class HostDomain {
-
-	public static String dbusername="root";
-	public static String dbpassword="integrationsrootdb@123";
-//	public static String dbpassword="root";
-    public static String url="jdbc:mysql://localhost/Enchant";
-}
