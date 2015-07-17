f7cf9284338bea458379b84f05db08620e583ae4
puru1joy <puru1joy@gmail.com>
2015-07-17 12:31:08 +0530

    checkin

--- Integrations/Basecamp/code/Bot/src/com/teamchat/integrations/basecamp/Universal.java
+++ Integrations/Basecamp/code/Bot/src/com/teamchat/integrations/basecamp/Universal.java
@@ -2,10 +2,6 @@ package com.teamchat.integrations.basecamp;
 
 //CONFIGURABLE DATA
 public class Universal {
-<<<<<<< cefdd4df35b3a36dd5220dc89db7b5afabc0d6da
-	public static String REDIRECT_URL = "http://integration.teamchat.com:8082/Basecamp_bot/Redirect_url", DB_NAME = "Basecamp",
-			DB_USERNAME = "webaroo", DB_PASSWORD = "webar00";
-=======
 	public static String
 			CLIENT_ID = "d48fa4605608e6bc3405232a71051aeb171eda35",
 			CLIENT_SECRET = "cd7f2cc27a364efe2b7299621a265a788d17a24a",
@@ -18,5 +14,4 @@ public class Universal {
 			DB_USERNAME = "tcinterns",
 			DB_PASSWORD = "PakyovBosh7",
 			TABLE_NAME = "basecamp_authorized";
->>>>>>> 057cb5fb307dcad9b6b0bd406174a4d86acea636
 }
