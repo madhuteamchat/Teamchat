96a44e2454a36e1995b2dc5d94a2b995c7a6e750
walaywarrior <anujarora.web2@gmail.com>
2015-07-17 21:28:06 +0530

    first commit...

---
+++ Integrations/glassdoor/src/com/teamchat/integration/glassdoor/bot/glassdoorintegrator.java
@@ -0,0 +1,41 @@
+package com.teamchat.integration.glassdoor.bot;
+
+import java.io.IOException;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+public class glassdoorintegrator {
+
+	public String getcompanies(String Keyword,String Location,int pn) throws IOException{
+	   	  
+	   	  String err = "Error";
+
+				OkHttpClient client = new OkHttpClient();
+				
+				//t.p = partnerid
+				//t.k = partner key
+				//q = paraphrase
+				//l = location
+				//ps = page size
+				//pn =page number
+				
+				Request request = new Request.Builder()
+				  .url("http://api.glassdoor.com/api/api.htm?t.p=39226&t.k=hl6voUXhoGA&format=json&v=1&action=employers&q="+Keyword+"&ps=2&pn="+pn+"&l="+Location)
+				  .get()
+				  .build();
+
+				Response response = client.newCall(request).execute();
+				
+				 if (!response.isSuccessful())
+				 {
+					 return err;
+					 
+				 }
+					 
+				 else
+				return response.body().string();
+		}
+	
+}
---
+++ Integrations/glassdoor/src/com/teamchat/integration/glassdoor/classes/Employer.java
@@ -0,0 +1,370 @@
+package com.teamchat.integration.glassdoor.classes;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+
+@Generated("org.jsonschema2pojo")
+public class Employer {
+
+@Expose
+private Integer id;
+@Expose
+private String name;
+@Expose
+private String website;
+@Expose
+private Boolean isEEP;
+@Expose
+private Boolean exactMatch;
+@Expose
+private String industry;
+@Expose
+private Integer numberOfRatings;
+@Expose
+private String squareLogo;
+@Expose
+private Double overallRating;
+@Expose
+private String ratingDescription;
+@Expose
+private String cultureAndValuesRating;
+@Expose
+private String seniorLeadershipRating;
+@Expose
+private String compensationAndBenefitsRating;
+@Expose
+private String careerOpportunitiesRating;
+@Expose
+private String workLifeBalanceRating;
+@Expose
+private String recommendToFriendRating;
+@Expose
+private FeaturedReview featuredReview;
+@Expose
+private Ceo ceo;
+
+/**
+*
+* @return
+* The id
+*/
+public Integer getId() {
+return id;
+}
+
+/**
+*
+* @param id
+* The id
+*/
+public void setId(Integer id) {
+this.id = id;
+}
+
+/**
+*
+* @return
+* The name
+*/
+public String getName() {
+return name;
+}
+
+/**
+*
+* @param name
+* The name
+*/
+public void setName(String name) {
+this.name = name;
+}
+
+/**
+*
+* @return
+* The website
+*/
+public String getWebsite() {
+return website;
+}
+
+/**
+*
+* @param website
+* The website
+*/
+public void setWebsite(String website) {
+this.website = website;
+}
+
+/**
+*
+* @return
+* The isEEP
+*/
+public Boolean getIsEEP() {
+return isEEP;
+}
+
+/**
+*
+* @param isEEP
+* The isEEP
+*/
+public void setIsEEP(Boolean isEEP) {
+this.isEEP = isEEP;
+}
+
+/**
+*
+* @return
+* The exactMatch
+*/
+public Boolean getExactMatch() {
+return exactMatch;
+}
+
+/**
+*
+* @param exactMatch
+* The exactMatch
+*/
+public void setExactMatch(Boolean exactMatch) {
+this.exactMatch = exactMatch;
+}
+
+/**
+*
+* @return
+* The industry
+*/
+public String getIndustry() {
+return industry;
+}
+
+/**
+*
+* @param industry
+* The industry
+*/
+public void setIndustry(String industry) {
+this.industry = industry;
+}
+
+/**
+*
+* @return
+* The numberOfRatings
+*/
+public Integer getNumberOfRatings() {
+return numberOfRatings;
+}
+
+/**
+*
+* @param numberOfRatings
+* The numberOfRatings
+*/
+public void setNumberOfRatings(Integer numberOfRatings) {
+this.numberOfRatings = numberOfRatings;
+}
+
+/**
+*
+* @return
+* The squareLogo
+*/
+public String getSquareLogo() {
+return squareLogo;
+}
+
+/**
+*
+* @param squareLogo
+* The squareLogo
+*/
+public void setSquareLogo(String squareLogo) {
+this.squareLogo = squareLogo;
+}
+
+/**
+*
+* @return
+* The overallRating
+*/
+public Double getOverallRating() {
+return overallRating;
+}
+
+/**
+*
+* @param overallRating
+* The overallRating
+*/
+public void setOverallRating(Double overallRating) {
+this.overallRating = overallRating;
+}
+
+/**
+*
+* @return
+* The ratingDescription
+*/
+public String getRatingDescription() {
+return ratingDescription;
+}
+
+/**
+*
+* @param ratingDescription
+* The ratingDescription
+*/
+public void setRatingDescription(String ratingDescription) {
+this.ratingDescription = ratingDescription;
+}
+
+/**
+*
+* @return
+* The cultureAndValuesRating
+*/
+public String getCultureAndValuesRating() {
+return cultureAndValuesRating;
+}
+
+/**
+*
+* @param cultureAndValuesRating
+* The cultureAndValuesRating
+*/
+public void setCultureAndValuesRating(String cultureAndValuesRating) {
+this.cultureAndValuesRating = cultureAndValuesRating;
+}
+
+/**
+*
+* @return
+* The seniorLeadershipRating
+*/
+public String getSeniorLeadershipRating() {
+return seniorLeadershipRating;
+}
+
+/**
+*
+* @param seniorLeadershipRating
+* The seniorLeadershipRating
+*/
+public void setSeniorLeadershipRating(String seniorLeadershipRating) {
+this.seniorLeadershipRating = seniorLeadershipRating;
+}
+
+/**
+*
+* @return
+* The compensationAndBenefitsRating
+*/
+public String getCompensationAndBenefitsRating() {
+return compensationAndBenefitsRating;
+}
+
+/**
+*
+* @param compensationAndBenefitsRating
+* The compensationAndBenefitsRating
+*/
+public void setCompensationAndBenefitsRating(String compensationAndBenefitsRating) {
+this.compensationAndBenefitsRating = compensationAndBenefitsRating;
+}
+
+/**
+*
+* @return
+* The careerOpportunitiesRating
+*/
+public String getCareerOpportunitiesRating() {
+return careerOpportunitiesRating;
+}
+
+/**
+*
+* @param careerOpportunitiesRating
+* The careerOpportunitiesRating
+*/
+public void setCareerOpportunitiesRating(String careerOpportunitiesRating) {
+this.careerOpportunitiesRating = careerOpportunitiesRating;
+}
+
+/**
+*
+* @return
+* The workLifeBalanceRating
+*/
+public String getWorkLifeBalanceRating() {
+return workLifeBalanceRating;
+}
+
+/**
+*
+* @param workLifeBalanceRating
+* The workLifeBalanceRating
+*/
+public void setWorkLifeBalanceRating(String workLifeBalanceRating) {
+this.workLifeBalanceRating = workLifeBalanceRating;
+}
+
+/**
+*
+* @return
+* The recommendToFriendRating
+*/
+public String getRecommendToFriendRating() {
+return recommendToFriendRating;
+}
+
+/**
+*
+* @param recommendToFriendRating
+* The recommendToFriendRating
+*/
+public void setRecommendToFriendRating(String recommendToFriendRating) {
+this.recommendToFriendRating = recommendToFriendRating;
+}
+
+/**
+*
+* @return
+* The featuredReview
+*/
+public FeaturedReview getFeaturedReview() {
+return featuredReview;
+}
+
+/**
+*
+* @param featuredReview
+* The featuredReview
+*/
+public void setFeaturedReview(FeaturedReview featuredReview) {
+this.featuredReview = featuredReview;
+}
+
+/**
+*
+* @return
+* The ceo
+*/
+public Ceo getCeo() {
+return ceo;
+}
+
+/**
+*
+* @param ceo
+* The ceo
+*/
+public void setCeo(Ceo ceo) {
+this.ceo = ceo;
+}
+
+}
