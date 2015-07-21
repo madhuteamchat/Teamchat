c8607b4c7b46fad8aab0e21554280fba6ad1044f
walaywarrior <anujarora.web2@gmail.com>
2015-07-21 22:04:54 +0530

    imple added..

--- Integrations/Glassdoor/Code/glassdoor/src/com/teamchat/integration/glassdoor/bot/glassdoorintegrator.java
+++ Integrations/Glassdoor/Code/glassdoor/src/com/teamchat/integration/glassdoor/bot/glassdoorintegrator.java
@@ -12,18 +12,17 @@ import com.squareup.okhttp.Response;
 public class glassdoorintegrator {
 // getting company details from glassdoor server.
 	public String getcompanies(String Keyword,String Location,int pn) throws IOException{
-	   	  
+   	  
 	   	  String err = "Error";
-
 				OkHttpClient client = new OkHttpClient();
-				
 				//t.p = partnerid
 				//t.k = partner key
 				//q = paraphrase
 				//l = location
 				//ps = page size
 				//pn =page number
-				
+				//t.p = 39226 (partner id)
+				//t.k = hl6voUXhoGA	(API key or partner key)
 				Request request = new Request.Builder()
 				  .url("http://api.glassdoor.com/api/api.htm?t.p=39226&t.k=hl6voUXhoGA&format=json&v=1&action=employers&q="+Keyword+"&ps=1&pn="+pn+"&l="+Location)
 				  .get()
@@ -34,11 +33,8 @@ public class glassdoorintegrator {
 				 if (!response.isSuccessful())
 				 {
 					 return err;
-					 
 				 }
-					 
 				 else
 				return response.body().string();
 		}
-	
 }
