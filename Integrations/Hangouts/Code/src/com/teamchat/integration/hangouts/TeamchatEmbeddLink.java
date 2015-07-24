cfa8e862ed48f2f90f70ee927e67eeed6698dd67
playitabhi <playitabhi@gmail.com>
2015-07-24 15:40:10 +0530

    Abhishek Nama: Hangouts Integration

---
+++ Integrations/Hangouts/Code/src/com/teamchat/integration/hangouts/TeamchatEmbeddLink.java
@@ -0,0 +1,24 @@
+package com.teamchat.integration.hangouts;
+
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class TeamchatEmbeddLink {
+	public static String createEmbeddedLink(String url, String title, String protocol) throws JSONException {
+		JSONObject object = new JSONObject();
+		JSONObject web = new JSONObject();
+		web.put("title", title);
+		web.put("cancelBtnName", "Back");
+		web.put("minWidth", "600");
+		web.put("draggable", "true");
+		web.put("newWindow", "true");
+		web.put("url", url);
+		object.put("web", web);
+		System.out.println(object.toString());
+		byte[] byteArray = Base64.encodeBase64(object.toString().getBytes());
+		String encodedString = new String(byteArray);
+		String fUrl = protocol + "://teamchat:data=" + encodedString;
+		return fUrl;
+	}
+}
