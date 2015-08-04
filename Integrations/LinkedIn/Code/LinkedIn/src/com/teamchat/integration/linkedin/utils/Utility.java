db18f2c0bfeede20f25090176ce1fb7e1de473ea
playitabhi <playitabhi@gmail.com>
2015-08-04 12:58:27 +0530

    Abhishek Nama: LinkedIn Updated

---
+++ Integrations/LinkedIn/Code/LinkedIn/src/com/teamchat/integration/linkedin/utils/Utility.java
@@ -0,0 +1,19 @@
+package com.teamchat.integration.linkedin.utils;
+
+public class Utility {
+	public static String shareDocHTML = "<div style=''> "
+				+ "<center> "
+					+ "<div style='margin-left:10px;'> "
+						+ "<img src='__imgUrl' style='margin-top:10px;margin-bottom:10px;margin-right:10px;' />"
+					+ "</div>"
+					+ "<div style=' margin-top:5px; color :DodgerBlue;font-family: monospace;'>"
+						+ "<div style='padding:2px;font-weight:bold'>__name</div>"
+						+ "<div style='padding:2px;color :#9B9B9B;'>__headline</div>"
+						+ "<div style='padding:2px;color :#9B9B9B;'>__cons</div>"
+						+ "<a href='__link' target='_blank' style='text-decoration: none;'>"
+							+ "<div style='background-color:DodgerBlue;padding:2px;color :white;'>__profile</div>"
+						+ "</a>"
+					+ "</div>"
+				+ "</center>"
+			+ "</div>";
+}
