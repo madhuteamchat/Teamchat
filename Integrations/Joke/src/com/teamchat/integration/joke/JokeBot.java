6c72ad8d6aec88787c8571d98c835ba344b49e79
playitabhi <playitabhi@gmail.com>
2015-08-12 17:21:15 +0530

    Abhishek Nama: News and Joke Integration

---
+++ Integrations/Joke/src/com/teamchat/integration/joke/JokeBot.java
@@ -0,0 +1,56 @@
+package com.teamchat.integration.joke;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+import com.teamchat.client.annotations.OnKeyword;
+import com.teamchat.client.sdk.TeamchatAPI;
+import com.teamchat.client.sdk.chatlets.PrimaryChatlet;
+import com.teamchat.client.sdk.chatlets.TextChatlet;
+
+public class JokeBot
+{
+	@OnKeyword("help")
+	public void help(TeamchatAPI api)
+	{
+		api.perform(api.context().currentRoom().post(new PrimaryChatlet().setQuestionHtml("Hi, i'm joke bot.<br/>enter 'joke' to laugh")));
+	}
+
+	@OnKeyword("joke")
+	public void joke(TeamchatAPI api) throws Exception
+	{
+		String url = "http://api.icndb.com/jokes/random";
+		//String url = "http://api.yomomma.info/";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		if (responseCode == 200)
+		{
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null)
+			{
+				response.append(inputLine);
+			}
+			in.close();
+
+			JSONObject json = new JSONObject(response.toString());
+
+			api.perform(api.context().currentRoom().post(new TextChatlet(json.getJSONObject("value").getString("joke"))));
+		}
+	}
+}
