f2bfd7863af3fe3afa35ebb7322ace4ee9cbf2f0
playitabhi <playitabhi@gmail.com>
2015-08-12 19:04:32 +0530

    a

--- Integrations/DisastersNews/src/com/teamchat/integration/disastersnews/Utility.java
+++
@@ -1,6 +0,0 @@
-package com.teamchat.integration.disastersnews;
-
-public class Utility
-{
-	public static final String help = "<center><img src='http://integration.teamchat.com/sol/bot-images/disaster-logo.jpg' width='150' /></center><h3 style='color:#159ceb'>Hi, i'm Disaster news Bot </h3><div><p>I give information about current disasters</p></div><div><p>Enter 'disaster' for getting information</p></div>";
-}
--- Integrations/Joke/src/com/teamchat/integration/joke/Utility.java
+++
@@ -1,6 +0,0 @@
-package com.teamchat.integration.joke;
-
-public class Utility
-{
-	public static final String help = "<center><img src='http://integration.teamchat.com/sol/bot-images/joke.png' width='150' /></center><h3 style='color:#159ceb'>Hi, i'm Joke Bot</h3><div><p>enter 'joke' to laugh</p></div>";
-}
