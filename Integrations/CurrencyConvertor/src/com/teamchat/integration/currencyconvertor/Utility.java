bc4caf21bf45f00b28cab6d1ca8983380202c9be
playitabhi <playitabhi@gmail.com>
2015-08-14 19:32:47 +0530

    Abhishek Nama: Currency Convertor

---
+++ Integrations/CurrencyConvertor/src/com/teamchat/integration/currencyconvertor/Utility.java
@@ -0,0 +1,6 @@
+package com.teamchat.integration.currencyconvertor;
+
+public class Utility
+{
+	public static final String help = "<center><img src='http://integration.teamchat.com/sol/bot-images/currency.png' width='150' /></center><h3 style='color:#159ceb'>Hi, i'm Currency Convertor Bot </h3><div></div><div><p>Enter 'convert' to convert currency</p></div>";
+}
